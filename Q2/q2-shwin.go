37b1b60944112e9a90838f34f80190e6558aad85
Yaritzasz <yaritzaaa25@gmail.com>
2025-09-17 14:20:34 -0400

    Fixed Q2

---
+++ Q2/q2-shwin.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Stack definition
+type Stack struct {
+	arr [100]int
+	top int
+}
+
+// Initialize a new stack
+func NewStack() *Stack {
+	return &Stack{top: -1}
+}
+
+// Push function
+func (s *Stack) Push(value int) {
+	if s.top >= len(s.arr)-1 {
+		fmt.Println("Error: Stack overflow")
+		return
+	}
+	s.top++
+	s.arr[s.top] = value
+}
+
+// Pop function
+func (s *Stack) Pop() int {
+	if s.top < 0 {
+		fmt.Println("Error: Stack underflow")
+		return -1 // sentinel value
+	}
+	val := s.arr[s.top]
+	s.top--
+	return val
+}
+
+// Demo
+func main() {
+	stack := NewStack()
+
+	stack.Push(10)
+	stack.Push(20)
+	stack.Push(30)
+
+	fmt.Println("Popped:", stack.Pop()) // 30
+	fmt.Println("Popped:", stack.Pop()) // 20
+	fmt.Println("Popped:", stack.Pop()) // 10
+	fmt.Println("Popped:", stack.Pop()) // Underflow
+}
