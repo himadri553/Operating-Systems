a465c4c70f1f9b87ff7789c61c0f928381c245d0
Shwinayy <164042247+Shwinayy@users.noreply.github.com>
2025-12-04 16:14:45 -0500

    Add RAID5 implementation with Write and Read methods
    Implement RAID5 structure with Write and Read methods for data storage and retrieval.

---
+++ hw7raid5.go
@@ -0,0 +1,57 @@
+package raid
+
+type RAID5 struct {
+    disks []*Disk
+}
+
+func NewRAID5(disks []*Disk) *RAID5 {
+    return &RAID5{disks}
+}
+
+func (r *RAID5) Write(block int, data []byte) error {
+    n := len(r.disks)
+    stripe := block / (n - 1)
+    pos := block % (n - 1)
+
+    parityDisk := stripe % n
+
+    dataDiskIndex := 0
+    for i := 0; i < n; i++ {
+        if i == parityDisk { continue }
+        if dataDiskIndex == pos {
+            // Write block here
+            if err := r.disks[i].WriteBlock(stripe, data); err != nil {
+                return err
+            }
+        }
+        dataDiskIndex++
+    }
+
+    // Recompute parity
+    parity := make([]byte, BlockSize)
+    for i := 0; i < n; i++ {
+        if i == parityDisk { continue }
+        b, _ := r.disks[i].ReadBlock(stripe)
+        parity = xorBlocks(parity, b)
+    }
+    return r.disks[parityDisk].WriteBlock(stripe, parity)
+}
+
+func (r *RAID5) Read(block int) ([]byte, error) {
+    n := len(r.disks)
+    stripe := block / (n - 1)
+    pos := block % (n - 1)
+
+    parityDisk := stripe % n
+
+    dataDiskIndex := 0
+    for i := 0; i < n; i++ {
+        if i == parityDisk { continue }
+        if dataDiskIndex == pos {
+            return r.disks[i].ReadBlock(stripe)
+        }
+        dataDiskIndex++
+    }
+
+    return nil, nil
+}
